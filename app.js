ab35f01d327f72d515df17e56eef782c3dc5bd46
Nkosi Phillip <internap627@gmail.com>
2019-05-12 15:50:57 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const http = require('http')
+
+const server = http.createServer((req,res) => {
+    console.log(req)
+    res.write('Hello World')
+    res.end()
+})
+
+server.listen(5000, () => console.log('server running....'))
