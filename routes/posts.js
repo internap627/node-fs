546a78edc609e53f0e7a649988999d5fc5d823bc
Nkosi Phillip <internap627@gmail.com>
2019-05-17 22:49:06 +0100

    Post model

--- routes/posts.js
+++ routes/posts.js
@@ -1,10 +1,46 @@
 const express = require('express')
-
 const router = express.Router()
+const Post = require('../models/Post')
 
-router.get('/', (req,res) => {
-    res.send('We are on posts')
+
+//Get all posts
+router.get('/', async (req,res) => {
+   
+   try { const posts = await Post.find()
+    res.json(posts)
+}
+    catch(err) {
+        res.json({message: err})
+    }
 })
+
+//Get one post
+router.get('/:postId', async (req, res) => {
+    try{
+        const post = await Post.findById(req.params.postId)
+        res.json(post)
+    }
+    catch (err) {
+        res.json({message: err})
+    }
+})
+
+//Create new post
+router.post('/', (req, res) => {
+    const post = new Post({
+        title: req.body.title,
+        description: req.body.description
+    })
+    post.save()
+    .then(data => {
+        res.json(data)
+    })
+    .catch(err => {
+        res.json({message: err})
+    })
+})
+
+
 
 module.exports = router
 
