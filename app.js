5d28cb03326a334fdd5bf99f0473767b1fcf4c69
Nkosi Phillip <internap627@gmail.com>
2019-05-18 04:02:49 +0100

    cors middleware

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 require('dotenv').config()
 
 app.use(bodyParser.json())
+app.use(cors())
 
 //Routes
 const postRoutes = require('./routes/posts.js')
